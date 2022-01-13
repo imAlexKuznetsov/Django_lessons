e954f19d3f14d3367d75afe6a566a4cf5a59f533
al <alenalexkuz@mail.com>
2022-01-13 19:34:34 +0300

    Site simplesite with bboard app with templates, layout, and css

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Bd, Rubric
+# IMPORT FOR FORMS
+from django.views.generic.edit import CreateView
+from .forms import BdForm
 
 
 # Create your views here.
@@ -9,9 +12,22 @@ def index(request):
     context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, 'bboard/index.html', context)
 
+
 def by_rubric(request, rubric_id):
     bbs = Bd.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+# CONTROLLER-CLASS FORM
+class BdCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BdForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
