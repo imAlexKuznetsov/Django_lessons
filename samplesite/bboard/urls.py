29c83d0b3892640259b991adef16ea45d085ffae
al <alenalexkuz@mail.com>
2022-01-27 14:39:59 +0300

    add class BdByRubricView(ListView), class BdAddView(FormView)

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import index, by_rubric, add_and_save, detail,  add,  BdCreateView
+from .views import index, by_rubric, add_and_save, detail,  add,  BdCreateView, BdByRubricView, BdDetailView
 
 
 app_name = 'bboard'
 urlpatterns = [
-    path('item/<int:bb_id>/', detail, name='detail'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    # path('item/<int:bb_id>/', detail, name='detail'),
+    # path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     # path('add/', add_and_save, name='add'),
     # path('add/save', add_save, name='add_save'),
     path('add/', BdCreateView.as_view(), name='add'),
+    path('<int:rubric_id>/', BdByRubricView.as_view(), name='by_rubric'),
+    path('detail/<int:pk>/', BdDetailView.as_view(), name='detail'),
     path('', index, name='index'),
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 
 
-
 from .models import Bd, Rubric
 # IMPORT FOR FORMS
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
 from .forms import BdForm
 
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+
+from django.views.generic.list import ListView
+
+
+############################################ main view   ######################################################
 def index(request):
     bbs = Bd.objects.all()
     rubrics = Rubric.objects.all()
@@ -19,16 +24,8 @@ def index(request):
     return render(request, 'bboard/index.html', context)
 
 
-def detail(request, bb_id):
-    try:
-        bb = Bd.objects.get(pk=bb_id)
-        context = {'bbs': bb}
-    except Bd.DoesNotExist:
-        return HttpResponseNotFound('Такое объявление не существует')
-    return render(request, 'bboard/item.html', context)
-
-
-def by_rubric(request, rubric_id):
+###########################################   view item filter by rubric   #######################################
+def by_rubric(request, rubric_id): # do not plug in project, it's alternate function class BdByRubricView
     bbs = Bd.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
@@ -36,19 +33,34 @@ def by_rubric(request, rubric_id):
     return render(request, 'bboard/by_rubric.html', context)
 
 
-# CONTROLLER-CLASS FORM
-class BdCreateView(CreateView):
-    template_name = 'bboard/create.html'
-    form_class = BdForm
-    # success_url = '/bboard/'
-    success_url = reverse_lazy('index')
+class BdByRubricView(ListView):
+    template_name = 'bboard/by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bd.objects.filter(rubric=self.kwargs['rubric_id'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['rubrics'] = Rubric.objects.all()
+        context['rubric'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=self.kwargs['rubric_id'])
         return context
 
 
+""" old version of class
+class BdByRubricView(TemplateView):
+    template_name = 'bboard/by_rubric.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['bbs'] = Bd.objects.filter(rubric=context['rubric_id'])
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
+        return context
+"""
+
+
+#############################################  add new items   ###########################################
 def add(request):
     bbf = BdForm()
     context = {'form': bbf}
@@ -65,3 +77,61 @@ def add_and_save(request):
         bbf = BdForm()
         context = {'form': bbf}
         return render(request, 'bboard/create.html', context)
+
+
+# CONTROLLER-CLASS FORM
+class BdCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BdForm
+    # success_url = '/bboard/'
+    success_url = reverse_lazy('index')
+    model = Bd
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+##############################################     details      ####################################
+def detail(request, bb_id):
+    try:
+        bb = Bd.objects.get(pk=bb_id)
+        context = {'bbs': bb}
+    except Bd.DoesNotExist:
+        return HttpResponseNotFound('Такое объявление не существует')
+    return render(request, 'bboard/item.html', context)
+
+
+class BdDetailView(DetailView):
+    # default template is "model_detail.html" -> bd_detail.html
+    model = Bd
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+######################################### form view ###############################################
+class BdAddView(FormView):
+    template_name = 'bboard/create.html'
+    form_class = BdForm
+    initial = {'price': 0.0}
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().fornm_valid(form)
+
+    def get_form(self, form_class=None):
+        self.object = super().get_form(form_class)
+        return self.object
+
+    def get_success_url(self):
+        return reverse('bboard:by_rubric', kwargs={'rubric_id': self.object.cleaned_data['rubric'].pk})
